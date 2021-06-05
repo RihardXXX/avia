13db5ef29909f40e7a8018eebd8e67ff74fba1da
RihardXXX <yusupa.akaeva@yandex.ru>
2021-06-05 15:19:18 +0300

    completed template

--- src/plugins/materialize.js
+++ src/plugins/materialize.js
@@ -5,6 +5,26 @@ import 'materialize-css/dist/js/materialize.min.js';
 const select = document.querySelectorAll('select');
 M.FormSelect.init(select);
 
+// функция возвращающая объект для работы с селектами
 export function getSelectInstance(elem) {
   return M.FormSelect.getInstance(elem);
+}
+
+// init autocomplete materialize
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete);
+
+// функция возаращающая объект для работы с автозаполнениями
+export function getAutocompleteInstance(elem) {
+  return M.Autocomplete.getInstance(elem);
+}
+
+// инициализация инпутов даты отправки и возврата
+const datepicker = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker, {
+  showClearBtn: true,
+});
+
+export function getDatepickerInstance(elem) {
+  return M.Datepicker.getInstance(elem);
 }
